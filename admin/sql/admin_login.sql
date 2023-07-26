fcafcb3bd52c91458d63353368641be85720091a
Denise Gozum <69788360+Xenoxianne@users.noreply.github.com>
2023-07-26 15:11:13 +0800

    SQL Files

--- admin/sql/admin_login.sql
+++ admin/sql/admin_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2023 at 02:15 PM
+-- Generation Time: Jul 26, 2023 at 09:09 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
--- admin/sql/course_info.sql
+++ admin/sql/course_info.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 23, 2023 at 10:03 AM
+-- Generation Time: Jul 26, 2023 at 09:09 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
